837cd580ea6358b93321205d641574d346fa35de
MD MASUM <md.wr.masum@gmail.com>
2021-02-20 21:20:21 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const CLIENT_ID = '500659744476-rr68mkvbp5dv9ooe299i27knln5l6bu2.apps.googleusercontent.com';
-  const SCOPE = "https://www.googleapis.com/auth/drive.metadata.readonly";
+  const CLIENT_ID = 'CLIENT_ID';
+  const SCOPE = "SCOPE";
 
   let auth2: gapi.auth2.GoogleAuth;
 
